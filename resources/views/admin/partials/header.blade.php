a44f6924c4e568715f272eac05bed9f614332a49
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-09-06 18:32:50 +0700

    refactor: header and sidebar components for improved readability and consistency

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,66 +1,80 @@
-<header class="sticky top-0 z-99999 flex w-full border-b border-gray-200 bg-white dark:border-gray-800 dark:bg-gray-900">
+<header
+    class="sticky top-0 z-99999 flex w-full border-b border-gray-200 bg-white dark:border-gray-800 dark:bg-gray-900">
     <div class="flex w-full items-center justify-between px-3 py-3 sm:px-5 sm:py-4 lg:px-6">
-        <!-- Kiri: Hamburger -->
+        <!-- hamburger -->
         <button
             class="flex h-10 w-10 items-center justify-center rounded-lg border border-gray-200 text-gray-500 transition-colors hover:bg-gray-100 dark:border-gray-800 dark:text-gray-400 dark:hover:bg-gray-800 lg:h-11 lg:w-11"
-            @click.stop="sidebarToggle = !sidebarToggle"
-        >
-            <svg
-                class="hidden fill-current lg:block"
-                width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"
-            >
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M0.583252 1C0.583252 0.585788 0.919038 0.25 1.33325 0.25H14.6666C15.0808 0.25 15.4166 0.585786 15.4166 1C15.4166 1.41421 15.0808 1.75 14.6666 1.75L1.33325 1.75C0.919038 1.75 0.583252 1.41422 0.583252 1ZM0.583252 11C0.583252 10.5858 0.919038 10.25 1.33325 10.25L14.6666 10.25C15.0808 10.25 15.4166 10.5858 15.4166 11C15.4166 11.4142 15.0808 11.75 14.6666 11.75L1.33325 11.75C0.919038 11.75 0.583252 11.4142 0.583252 11ZM1.33325 5.25C0.919038 5.25 0.583252 5.58579 0.583252 6C0.583252 6.41421 0.919038 6.75 1.33325 6.75L7.99992 6.75C8.41413 6.75 8.74992 6.41421 8.74992 6C8.74992 5.58579 8.41413 5.25 7.99992 5.25L1.33325 5.25Z" fill="currentColor"/>
+            @click.stop="sidebarToggle = !sidebarToggle">
+            <svg class="hidden fill-current lg:block" width="16" height="12" viewBox="0 0 16 12" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M0.583252 1C0.583252 0.585788 0.919038 0.25 1.33325 0.25H14.6666C15.0808 0.25 15.4166 0.585786 15.4166 1C15.4166 1.41421 15.0808 1.75 14.6666 1.75L1.33325 1.75C0.919038 1.75 0.583252 1.41422 0.583252 1ZM0.583252 11C0.583252 10.5858 0.919038 10.25 1.33325 10.25L14.6666 10.25C15.0808 10.25 15.4166 10.5858 15.4166 11C15.4166 11.4142 15.0808 11.75 14.6666 11.75L1.33325 11.75C0.919038 11.75 0.583252 11.4142 0.583252 11ZM1.33325 5.25C0.919038 5.25 0.583252 5.58579 0.583252 6C0.583252 6.41421 0.919038 6.75 1.33325 6.75L7.99992 6.75C8.41413 6.75 8.74992 6.41421 8.74992 6C8.74992 5.58579 8.41413 5.25 7.99992 5.25L1.33325 5.25Z"
+                    fill="currentColor" />
             </svg>
-            <svg
-                class="fill-current lg:hidden"
-                width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-            >
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M3.25 6C3.25 5.58579 3.58579 5.25 4 5.25L20 5.25C20.4142 5.25 20.75 5.58579 20.75 6C20.75 6.41421 20.4142 6.75 20 6.75L4 6.75C3.58579 6.75 3.25 6.41422 3.25 6ZM3.25 18C3.25 17.5858 3.58579 17.25 4 17.25L20 17.25C20.4142 17.25 20.75 17.5858 20.75 18C20.75 18.4142 20.4142 18.75 20 18.75L4 18.75C3.58579 18.75 3.25 18.4142 3.25 18ZM4 11.25C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75L12 12.75C12.4142 12.75 12.75 12.4142 12.75 12C12.75 11.5858 12.4142 11.25 12 11.25L4 11.25Z" fill="currentColor"/>
+            <svg class="fill-current lg:hidden" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M3.25 6C3.25 5.58579 3.58579 5.25 4 5.25L20 5.25C20.4142 5.25 20.75 5.58579 20.75 6C20.75 6.41421 20.4142 6.75 20 6.75L4 6.75C3.58579 6.75 3.25 6.41422 3.25 6ZM3.25 18C3.25 17.5858 3.58579 17.25 4 17.25L20 17.25C20.4142 17.25 20.75 17.5858 20.75 18C20.75 18.4142 20.4142 18.75 20 18.75L4 18.75C3.58579 18.75 3.25 18.4142 3.25 18ZM4 11.25C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75L12 12.75C12.4142 12.75 12.75 12.4142 12.75 12C12.75 11.5858 12.4142 11.25 12 11.25L4 11.25Z"
+                    fill="currentColor" />
             </svg>
         </button>
 
-        <!-- Kanan: Dark mode toggle + User -->
         <div class="flex items-center gap-3">
-            <!-- Dark Mode Toggle -->
+            <!-- dark mode toggle -->
             <button
                 class="relative flex h-10 w-10 items-center justify-center rounded-full border border-gray-200 bg-white text-gray-500 transition-colors hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-white lg:h-11 lg:w-11"
                 @click.prevent="darkMode = !darkMode"
                 x-data="{ darkMode: document.documentElement.classList.contains('dark') }"
                 x-init="document.documentElement.classList.toggle('dark', darkMode)"
-                x-effect="document.documentElement.classList.toggle('dark', darkMode)"
-            >
-                <svg x-show="darkMode" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="fill-current">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M9.99998 1.5415C10.4142 1.5415 10.75 1.87729 10.75 2.2915V3.5415C10.75 3.95572 10.4142 4.2915 9.99998 4.2915C9.58577 4.2915 9.24998 3.95572 9.24998 3.5415V2.2915C9.24998 1.87729 9.58577 1.5415 9.99998 1.5415ZM10.0009 6.79327C8.22978 6.79327 6.79402 8.22904 6.79402 10.0001C6.79402 11.7712 8.22978 13.207 10.0009 13.207C11.772 13.207 13.2078 11.7712 13.2078 10.0001C13.2078 8.22904 11.772 6.79327 10.0009 6.79327ZM5.29402 10.0001C5.29402 7.40061 7.40135 5.29327 10.0009 5.29327C12.6004 5.29327 14.7078 7.40061 14.7078 10.0001C14.7078 12.5997 12.6004 14.707 10.0009 14.707C7.40135 14.707 5.29402 12.5997 5.29402 10.0001Z"/>
+                x-effect="document.documentElement.classList.toggle('dark', darkMode)">
+                <svg x-show="darkMode" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                    xmlns="http://www.w3.org/2000/svg" class="fill-current">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M9.99998 1.5415C10.4142 1.5415 10.75 1.87729 10.75 2.2915V3.5415C10.75 3.95572 10.4142 4.2915 9.99998 4.2915C9.58577 4.2915 9.24998 3.95572 9.24998 3.5415V2.2915C9.24998 1.87729 9.58577 1.5415 9.99998 1.5415ZM10.0009 6.79327C8.22978 6.79327 6.79402 8.22904 6.79402 10.0001C6.79402 11.7712 8.22978 13.207 10.0009 13.207C11.772 13.207 13.2078 11.7712 13.2078 10.0001C13.2078 8.22904 11.772 6.79327 10.0009 6.79327ZM5.29402 10.0001C5.29402 7.40061 7.40135 5.29327 10.0009 5.29327C12.6004 5.29327 14.7078 7.40061 14.7078 10.0001C14.7078 12.5997 12.6004 14.707 10.0009 14.707C7.40135 14.707 5.29402 12.5997 5.29402 10.0001Z" />
                 </svg>
-                <svg x-show="!darkMode" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="fill-current">
-                    <path d="M17.4547 11.97L18.1799 12.1611C18.265 11.8383 18.1265 11.4982 17.8401 11.3266C17.5538 11.1551 17.1885 11.1934 16.944 11.4207L17.4547 11.97ZM8.0306 2.5459L8.57989 3.05657C8.80718 2.81209 8.84554 2.44682 8.67398 2.16046C8.50243 1.8741 8.16227 1.73559 7.83948 1.82066L8.0306 2.5459ZM12.9154 13.0035C9.64678 13.0035 6.99707 10.3538 6.99707 7.08524H5.49707C5.49707 11.1823 8.81835 14.5035 12.9154 14.5035V13.0035ZM16.944 11.4207C15.8869 12.4035 14.4721 13.0035 12.9154 13.0035V14.5035C14.8657 14.5035 16.6418 13.7499 17.9654 12.5193L16.944 11.4207ZM16.7295 11.7789C15.9437 14.7607 13.2277 16.9586 10.0003 16.9586V18.4586C13.9257 18.4586 17.2249 15.7853 18.1799 12.1611L16.7295 11.7789ZM10.0003 16.9586C6.15734 16.9586 3.04199 13.8433 3.04199 10.0003H1.54199C1.54199 14.6717 5.32892 18.4586 10.0003 18.4586V16.9586ZM3.04199 10.0003C3.04199 6.77289 5.23988 4.05695 8.22173 3.27114L7.83948 1.82066C4.21532 2.77574 1.54199 6.07486 1.54199 10.0003H3.04199ZM6.99707 7.08524C6.99707 5.52854 7.5971 4.11366 8.57989 3.05657C7.48132 2.03522C6.25073 3.35885 5.49707 5.13487 5.49707 7.08524H6.99707Z"/>
+                <svg x-show="!darkMode" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                    xmlns="http://www.w3.org/2000/svg" class="fill-current">
+                    <path
+                        d="M17.4547 11.97L18.1799 12.1611C18.265 11.8383 18.1265 11.4982 17.8401 11.3266C17.5538 11.1551 17.1885 11.1934 16.944 11.4207L17.4547 11.97ZM8.0306 2.5459L8.57989 3.05657C8.80718 2.81209 8.84554 2.44682 8.67398 2.16046C8.50243 1.8741 8.16227 1.73559 7.83948 1.82066L8.0306 2.5459ZM12.9154 13.0035C9.64678 13.0035 6.99707 10.3538 6.99707 7.08524H5.49707C5.49707 11.1823 8.81835 14.5035 12.9154 14.5035V13.0035ZM16.944 11.4207C15.8869 12.4035 14.4721 13.0035 12.9154 13.0035V14.5035C14.8657 14.5035 16.6418 13.7499 17.9654 12.5193L16.944 11.4207ZM16.7295 11.7789C15.9437 14.7607 13.2277 16.9586 10.0003 16.9586V18.4586C13.9257 18.4586 17.2249 15.7853 18.1799 12.1611L16.7295 11.7789ZM10.0003 16.9586C6.15734 16.9586 3.04199 13.8433 3.04199 10.0003H1.54199C1.54199 14.6717 5.32892 18.4586 10.0003 18.4586V16.9586ZM3.04199 10.0003C3.04199 6.77289 5.23988 4.05695 8.22173 3.27114L7.83948 1.82066C4.21532 2.77574 1.54199 6.07486 1.54199 10.0003H3.04199ZM6.99707 7.08524C6.99707 5.52854 7.5971 4.11366 8.57989 3.05657C7.48132 2.03522C6.25073 3.35885 5.49707 5.13487 5.49707 7.08524H6.99707Z" />
                 </svg>
             </button>
 
-            <!-- User Area -->
+            <!-- user area -->
             @auth
             <div class="relative" x-data="{ userMenuOpen: false }" @click.outside="userMenuOpen = false">
-                <a class="flex items-center text-gray-700 dark:text-gray-400" href="#" @click.prevent="userMenuOpen = !userMenuOpen">
-                    <span class="flex h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-brand-100 text-sm font-semibold text-brand-700 dark:bg-white/10 dark:text-brand-400 lg:h-11 lg:w-11">
+                <a class="flex items-center text-gray-700 dark:text-gray-400" href="#"
+                    @click.prevent="userMenuOpen = !userMenuOpen">
+                    <span
+                        class="flex h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-brand-100 text-sm font-semibold text-brand-700 dark:bg-white/10 dark:text-brand-400 lg:h-11 lg:w-11">
                         {{ strtoupper(substr(auth()->user()->name ?? 'A', 0, 1)) }}
                     </span>
                     <span class="hidden ml-2 text-sm font-medium sm:block">{{ auth()->user()->name }}</span>
-                    <svg :class="userMenuOpen && 'rotate-180'" class="ml-1 h-4 w-4 stroke-gray-500 dark:stroke-gray-400" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M4.3125 8.65625L9 13.3437L13.6875 8.65625" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <svg :class="userMenuOpen && 'rotate-180'" class="ml-1 h-4 w-4 stroke-gray-500 dark:stroke-gray-400"
+                        viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.3125 8.65625L9 13.3437L13.6875 8.65625" stroke="currentColor" stroke-width="1.5"
+                            stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
                 </a>
 
-                <!-- Dropdown -->
-                <div x-show="userMenuOpen" x-transition x-cloak class="absolute right-0 z-99999 mt-4.25 w-64 flex flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-900">
+                <!-- dropdown -->
+                <div x-show="userMenuOpen" x-transition x-cloak
+                    class="absolute right-0 z-99999 mt-4.25 w-64 flex flex-col rounded-2xl border border-gray-200 bg-white p-3 shadow-theme-lg dark:border-gray-800 dark:bg-gray-900">
                     <div class="border-b border-gray-200 pb-3 dark:border-gray-800">
-                        <span class="block text-sm font-medium text-gray-700 dark:text-gray-400">{{ auth()->user()->name }}</span>
-                        <span class="mt-0.5 block text-xs text-gray-500 dark:text-gray-400">{{ auth()->user()->email }}</span>
+                        <span class="block text-sm font-medium text-gray-700 dark:text-gray-400">{{ auth()->user()->name
+                            }}</span>
+                        <span class="mt-0.5 block text-xs text-gray-500 dark:text-gray-400">{{ auth()->user()->email
+                            }}</span>
                     </div>
                     <form method="post" action="{{ filament()->getLogoutUrl() }}" class="mt-3">
                         @csrf
-                        <button type="submit" class="group flex w-full items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-white/5">
-                            <svg class="fill-gray-500 group-hover:fill-gray-700 dark:fill-gray-400 dark:group-hover:fill-gray-300" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M15.1007 19.247C14.6865 19.247 14.3507 18.9112 14.3507 18.497L14.3507 14.245H12.8507V18.497C12.8507 19.7396 13.8581 20.747 15.1007 20.747H18.5007C19.7434 20.747 20.7507 19.7396 20.7507 18.497L20.7507 5.49609C20.7507 4.25345 19.7433 3.24609 18.5007 3.24609H15.1007C13.8581 3.24609 12.8507 4.25345 12.8507 5.49609V9.74501L14.3507 9.74501V5.49609C14.3507 5.08188 14.6865 4.74609 15.1007 4.74609L18.5007 4.74609C18.9149 4.74609 19.2507 5.08188 19.2507 5.49609L19.2507 18.497C19.2507 18.9112 18.9149 19.247 18.5007 19.247H15.1007ZM3.25073 11.9984C3.25073 12.2144 3.34204 12.4091 3.48817 12.546L8.09483 17.1556C8.38763 17.4485 8.86251 17.4487 9.15549 17.1559C9.44848 16.8631 9.44863 16.3882 9.15583 16.0952L5.81116 12.7484L16.0007 12.7484C16.4149 12.7484 16.7507 12.4127 16.7507 11.9984C16.7507 11.5842 16.4149 11.2484 16.0007 11.2484L5.81528 11.2484L9.15585 7.90554C9.44864 7.61255 9.44847 7.13767 9.15547 6.84488C8.86248 6.55209 8.3876 6.55226 8.09481 6.84525L3.52309 11.4202C3.35673 11.5577 3.25073 11.7657 3.25073 11.9984Z" fill="currentColor"/>
+                        <button type="submit"
+                            class="group flex w-full items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-white/5">
+                            <svg class="fill-gray-500 group-hover:fill-gray-700 dark:fill-gray-400 dark:group-hover:fill-gray-300"
+                                width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M15.1007 19.247C14.6865 19.247 14.3507 18.9112 14.3507 18.497L14.3507 14.245H12.8507V18.497C12.8507 19.7396 13.8581 20.747 15.1007 20.747H18.5007C19.7434 20.747 20.7507 19.7396 20.7507 18.497L20.7507 5.49609C20.7507 4.25345 19.7433 3.24609 18.5007 3.24609H15.1007C13.8581 3.24609 12.8507 4.25345 12.8507 5.49609V9.74501L14.3507 9.74501V5.49609C14.3507 5.08188 14.6865 4.74609 15.1007 4.74609L18.5007 4.74609C18.9149 4.74609 19.2507 5.08188 19.2507 5.49609L19.2507 18.497C19.2507 18.9112 18.9149 19.247 18.5007 19.247H15.1007ZM3.25073 11.9984C3.25073 12.2144 3.34204 12.4091 3.48817 12.546L8.09483 17.1556C8.38763 17.4485 8.86251 17.4487 9.15549 17.1559C9.44848 16.8631 9.44863 16.3882 9.15583 16.0952L5.81116 12.7484L16.0007 12.7484C16.4149 12.7484 16.7507 12.4127 16.7507 11.9984C16.7507 11.5842 16.4149 11.2484 16.0007 11.2484L5.81528 11.2484L9.15585 7.90554C9.44864 7.61255 9.44847 7.13767 9.15547 6.84488C8.86248 6.55209 8.3876 6.55226 8.09481 6.84525L3.52309 11.4202C3.35673 11.5577 3.25073 11.7657 3.25073 11.9984Z"
+                                    fill="currentColor" />
                             </svg>
                             <span>Logout</span>
                         </button>
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
         sidebarHover ? 'sidebar-hover' : '',
     ]" @mouseenter="if (sidebarToggle && isDesktop) sidebarHover = true" @mouseleave="sidebarHover = false"
     @resize.window="isDesktop = window.innerWidth >= 1280"
-    class="sidebar fixed left-0 top-0 z-999999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 duration-300 ease-linear dark:border-gray-800 dark:bg-gray-900 lg:translate-x-0 lg:px-2"
+    class="sidebar fixed left-0 top-0 z-999999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 duration-300 ease-linear dark:border-gray-800 dark:bg-gray-900 lg:translate-x-0"
     @click.outside="if (window.innerWidth < 1280) sidebarToggle = false">
     <!-- SIDEBAR HEADER -->
     <div :class="sidebarToggle && isDesktop ? 'justify-center' : 'justify-between'"
@@ -16,9 +16,9 @@
     </div>
 
     <div class="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
-        <nav>
+        <nav class="sidebar-nav flex flex-1 flex-col gap-4 pb-6">
             <div>
-                <h3 class="mb-4 text-xs uppercase leading-[20px] text-gray-400">
+                <h3 class="mb-2 text-xs uppercase leading-[20px] text-gray-400">
                     <span class="menu-group-title" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">MENU</span>
                 </h3>
 
@@ -33,14 +33,15 @@
                                     d="M5.5 3.25C4.25736 3.25 3.25 4.25736 3.25 5.5V8.99998C3.25 10.2426 4.25736 11.25 5.5 11.25H9C10.2426 11.25 11.25 10.2426 11.25 8.99998V5.5C11.25 4.25736 10.2426 3.25 9 3.25H5.5ZM4.75 5.5C4.75 5.08579 5.08579 4.75 5.5 4.75H9C9.41421 4.75 9.75 5.08579 9.75 5.5V8.99998C9.75 9.41419 9.41421 9.74998 9 9.74998H5.5C5.08579 9.74998 4.75 9.41419 4.75 8.99998V5.5ZM5.5 12.75C4.25736 12.75 3.25 13.7574 3.25 15V18.5C3.25 19.7426 4.25736 20.75 5.5 20.75H9C10.2426 20.75 11.25 19.7427 11.25 18.5V15C11.25 13.7574 10.2426 12.75 9 12.75H5.5ZM4.75 15C4.75 14.5858 5.08579 14.25 5.5 14.25H9C9.41421 14.25 9.75 14.5858 9.75 15V18.5C9.75 18.9142 9.41421 19.25 9 19.25H5.5C5.08579 19.25 4.75 18.9142 4.75 18.5V15ZM12.75 5.5C12.75 4.25736 13.7574 3.25 15 3.25H18.5C19.7426 3.25 20.75 4.25736 20.75 5.5V8.99998C20.75 10.2426 19.7426 11.25 18.5 11.25H15C13.7574 11.25 12.75 10.2426 12.75 8.99998V5.5ZM15 4.75C14.5858 4.75 14.25 5.08579 14.25 5.5V8.99998C14.25 9.41419 14.5858 9.74998 15 9.74998H18.5C18.9142 9.74998 19.25 9.41419 19.25 8.99998V5.5C19.25 5.08579 18.9142 4.75 18.5 4.75H15ZM15 12.75C13.7574 12.75 12.75 13.7574 12.75 15V18.5C12.75 19.7426 13.7574 20.75 15 20.75H18.5C19.7426 20.75 20.75 19.7427 20.75 18.5V15C20.75 13.7574 19.7426 12.75 18.5 12.75H15ZM14.25 15C14.25 14.5858 14.5858 14.25 15 14.25H18.5C18.9142 14.25 19.25 14.5858 19.25 15V18.5C19.25 18.9142 18.9142 19.25 18.5 19.25H15C14.5858 19.25 14.25 18.9142 14.25 18.5V15Z"
                                     fill="currentColor" />
                             </svg>
-                            <span class="menu-item-text" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Dashboard</span>
+                            <span class="menu-item-text"
+                                :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Dashboard</span>
                         </a>
                     </li>
                 </ul>
             </div>
 
             <div>
-                <h3 class="mb-4 text-xs uppercase leading-[20px] text-gray-400">
+                <h3 class="mb-2 text-xs uppercase leading-[20px] text-gray-400">
                     <span class="menu-group-title" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Content</span>
                 </h3>
                 <ul class="mb-6 flex flex-col gap-1.5">
@@ -54,7 +55,8 @@
                                     d="M12 5.25C9.586 5.25 7.586 5.25 5.25 5.25C4.007 5.25 3.25 6.007 3.25 7.5V16.5C3.25 17.743 4.007 18.5 5.25 18.5C7.586 18.5 9.586 18.5 12 18.5C14.414 18.5 16.414 18.5 18.75 18.5C19.993 18.5 20.75 17.743 20.75 16.5V7.5C20.75 6.007 19.993 5.25 18.75 5.25C16.414 5.25 14.414 5.25 12 5.25ZM12 17C9.243 17 7.03 16.993 4.75 16.972V7.528C7.03 7.507 9.243 7.5 12 7.5C14.757 7.5 16.97 7.507 19.25 7.528V16.972C16.97 16.993 14.757 17 12 17Z"
                                     fill="currentColor" />
                             </svg>
-                            <span class="menu-item-text" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Courses</span>
+                            <span class="menu-item-text"
+                                :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Courses</span>
                         </a>
                     </li>
                     <li>
@@ -67,7 +69,8 @@
                                     d="M3.5 8.187V17.25C3.5 17.6642 3.83579 18 4.25 18H19.75C20.1642 18 20.5 17.6642 20.5 17.25V8.18747L13.2873 13.2171C12.5141 13.7563 11.4866 13.7563 10.7134 13.2171L3.5 8.187ZM20.5 6.2286C20.5 6.23039 20.5 6.23218 20.5 6.23398V6.24336C20.4976 6.31753 20.4604 6.38643 20.3992 6.42905L12.4293 11.9867C12.1716 12.1664 11.8291 12.1664 11.5713 11.9867L3.60116 6.42885C3.538 6.38481 3.50035 6.31268 3.50032 6.23568C3.50028 6.10553 3.60577 6 3.73592 6H20.2644C20.3922 6 20.4963 6.10171 20.5 6.2286ZM22 6.25648V17.25C22 18.4926 20.9926 19.5 19.75 19.5H4.25C3.00736 19.5 2 18.4926 2 17.25V6.23398C2 6.22371 2.00021 6.2135 2.00061 6.20333C2.01781 5.25971 2.78812 4.5 3.73592 4.5H20.2644C21.2229 4.5 22 5.27697 22.0001 6.23549C22.0001 6.24249 22.0001 6.24949 22 6.25648Z"
                                     fill="currentColor" />
                             </svg>
-                            <span class="menu-item-text" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Units</span>
+                            <span class="menu-item-text"
+                                :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Units</span>
                         </a>
                     </li>
                     <li>
@@ -80,7 +83,8 @@
                                     d="M10.5 3.75C10.5 3.33579 10.8358 3 11.25 3H19.5C19.9142 3 20.25 3.33579 20.25 3.75C20.25 4.16421 19.9142 4.5 19.5 4.5H11.25C10.8358 4.5 10.5 4.16421 10.5 3.75ZM10.5 7.75C10.5 7.33579 10.8358 7 11.25 7H19.5C19.9142 7 20.25 7.33579 20.25 7.75C20.25 8.16421 19.9142 8.5 19.5 8.5H11.25C10.8358 8.5 10.5 8.16421 10.5 7.75ZM10.5 11.75C10.5 11.3358 10.8358 11 11.25 11H19.5C19.9142 11 20.25 11.3358 20.25 11.75C20.25 12.1642 19.9142 12.5 19.5 12.5H11.25C10.8358 12.5 10.5 12.1642 10.5 11.75ZM7 3.75C7 4.16421 6.66421 4.5 6.25 4.5H5.5C5.08579 4.5 4.75 4.83579 4.75 5.25V19.25C4.75 19.6642 5.08579 20 5.5 20H18.5C18.9142 20 19.25 19.6642 19.25 19.25V18.5C19.25 18.0858 19.5858 17.75 20 17.75C20.4142 17.75 20.75 18.0858 20.75 18.5V19.25C20.75 20.4926 19.7426 21.5 18.5 21.5H5.5C4.25736 21.5 3.25 20.4926 3.25 19.25V5.25C3.25 4.00736 4.25736 3 5.5 3H6.25C6.66421 3 7 3.33579 7 3.75ZM12.25 19.75C12.25 20.1642 11.9142 20.5 11.5 20.5C11.0858 20.5 10.75 20.1642 10.75 19.75V18.25C10.75 17.8358 11.0858 17.5 11.5 17.5C11.9142 17.5 12.25 17.8358 12.25 18.25V19.75Z"
                                     fill="currentColor" />
                             </svg>
-                            <span class="menu-item-text" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Lessons</span>
+                            <span class="menu-item-text"
+                                :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Lessons</span>
                         </a>
                     </li>
                     <li>
@@ -93,14 +97,15 @@
                                     d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 5.75C12.4142 5.75 12.75 6.08579 12.75 6.5V12C12.75 12.4142 12.4142 12.75 12 12.75C11.5858 12.75 11.25 12.4142 11.25 12V6.5C11.25 6.08579 11.5858 5.75 12 5.75ZM12 14.75C12.4142 14.75 12.75 15.0858 12.75 15.5C12.75 15.9142 12.4142 16.25 12 16.25C11.5858 16.25 11.25 15.9142 11.25 15.5C11.25 15.0858 11.5858 14.75 12 14.75Z"
                                     fill="currentColor" />
                             </svg>
-                            <span class="menu-item-text" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Questions</span>
+                            <span class="menu-item-text"
+                                :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Questions</span>
                         </a>
                     </li>
                 </ul>
             </div>
 
             <div>
-                <h3 class="mb-4 text-xs uppercase leading-[20px] text-gray-400">
+                <h3 class="mb-2 text-xs uppercase leading-[20px] text-gray-400">
                     <span class="menu-group-title" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">User</span>
                 </h3>
                 <ul class="mb-6 flex flex-col gap-1.5">
@@ -114,7 +119,8 @@
                                     d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM8.5 10.5C8.5 8.567 10.067 7 12 7C13.933 7 15.5 8.567 15.5 10.5C15.5 12.433 13.933 14 12 14C10.067 14 8.5 12.433 8.5 10.5ZM12 20.5C9.189 20.5 6.66 19.13 5.126 17C5.863 14.89 7.89 13.5 10.25 13.5H13.75C16.11 13.5 18.137 14.89 18.874 17C17.34 19.13 14.811 20.5 12 20.5Z"
                                     fill="currentColor" />
                             </svg>
-                            <span class="menu-item-text" :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Users</span>
+                            <span class="menu-item-text"
+                                :class="sidebarToggle && isDesktop ? 'lg:hidden' : ''">Users</span>
                         </a>
                     </li>
                 </ul>
